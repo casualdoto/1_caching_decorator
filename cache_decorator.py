91523cf13452a7950f86edfa3b8b6005be030cd6
shmel9va <168944627+shmel9va@users.noreply.github.com>
2024-11-11 00:13:08 +0300

    Added function caching decorator with default parameters
    Implemented `cache_decorator` to cache function results, supporting customizable strategies (default LRU), cache depth, and TTL

--- cache_decorator.py
+++ cache_decorator.py
@@ -1,4 +1,6 @@
 import time
+from functools import wraps
+
 
 class CacheManager:
     def __init__(self, strategy, cache_depth, ttl):
@@ -28,3 +30,29 @@ class CacheManager:
             """Закомментировано для тестирования (будет добавлено третьим человеком)
             # self._add_to_fifo_cache(key, value)"""
 
+
+def cache_decorator(strategy='LRU', cache_depth=10, ttl=60): # Значения по умолчанию
+    """Декоратор для кэширования результатов функций."""
+    cache_managers = {}
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # Создание уникального ключа для каждого вызова функции
+            key = (args, frozenset(kwargs.items()))
+            if func not in cache_managers:
+                cache_managers[func] = CacheManager(strategy, cache_depth, ttl)
+
+            cache_manager = cache_managers[func]
+            cached_result = cache_manager.get_cache(key)
+            if cached_result is not None:
+                return cached_result
+
+            # Вычисление результата и кэширование
+            result = func(*args, **kwargs)
+            cache_manager.set_cache(key, result)
+            return result
+
+        return wrapper
+
+    return decorator
